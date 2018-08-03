b4b5e29b5a594d5b0a53e80d837de20ed0ce83da
Greilfang <565222945@qq.com>
2018-08-03 18:34:16 +0800

    实现评论在屏幕上的显示

--- 评论功能实现/src/Comment.js
+++ 评论功能实现/src/Comment.js
@@ -2,9 +2,13 @@ import React ,{Component} from "react"
 class Comment extends Component{
     render(){
         return(
-            <div>
-                Comment
+            <div className='comment'>
+                <div className='comment-user'>
+                    <span>{this.props.comment.username}</span>
+                </div>
+                <p>{this.props.comment.content}</p>
             </div>
         )
     }
 }
+export default Comment
--- 评论功能实现/src/CommentList.js
+++ 评论功能实现/src/CommentList.js
@@ -1,8 +1,16 @@
 import React,{Component} from "react"
+import Comment from "./Comment"
 class CommentList extends Component{
     render(){
+        const comments=[
+            {username:"Tom",content:"I am a good man"},
+            {username:"Mary",content:"Just so so"},
+            {username:"Jack",content:"Fuck you off"}
+        ]
         return(
-            <div>CommentList</div>
+          <div>
+              {comments.map((comment,i)=> <Comment comment={comment} key={i}/>)}
+          </div>
         )
     }
 }
